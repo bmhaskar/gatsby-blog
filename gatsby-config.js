05138cff3ecf639744efdb80deb8c53a62e1f6e1
Bharat Mhaskar <bmhaskar@gmail.com>
2019-01-10 20:32:36 +0530

    Added icon and changed the theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,10 +32,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000000`,
+        theme_color: `#00a562`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/blog-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
